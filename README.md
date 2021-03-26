d3ee521dc38b6e14ee926c926bb35edb74b8deb6
dbennett_tier2 <dbennett@tier2consulting.com>
2021-03-26 17:58:42 +0000

    [major] Changed name and fixed bugs to get it working

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Wox.DSSH
+# Tool.Connect2 
+Helper plugin for me to launch SSH commands easily from Wox
--- main.py
+++ main.py
@@ -1,32 +1,23 @@
-#encoding=utf8
-from wox import Wox,WoxAPI
+from wox import Wox
 import os
 
-#Your class must inherit from Wox base class https://github.com/qianlifeng/Wox/blob/master/PythonHome/wox.py
-#The wox class here did some works to simplify the communication between Wox and python plugin.
 class Main(Wox):
 
-  def request(self,url):
-    #If user set the proxy, you should handle it.
-    if self.proxy and self.proxy.get("enabled") and self.proxy.get("server"):
-      proxies = {
-        "http":"http://{}:{}".format(self.proxy.get("server"),self.proxy.get("port")),
-        "https":"http://{}:{}".format(self.proxy.get("server"),self.proxy.get("port"))}
-      return requests.get(url,proxies = proxies)
-    else:
-      return requests.get(url)
+  def query(self, userInput):
+    return [{
+      'Title': 'Enter server name',
+      'SubTitle': 'Connect to: ' + userInput,
+      'IcoPath': 'Images/pic.png',
+      'JsonRPCAction': {
+        'method': 'action',
+        'parameters': [userInput],
+        'dontHideAfterAction': False
+      }
+    }]
 
-  # A function named query is necessary, we will automatically invoke this function when user query this plugin
-  def query(self,key):
+  def action(self, serverName, commandType = 'ssh'):
+    if commandType == 'ssh':
+      os.system('wt -p "{0caa0dad-35be-5f56-a8ff-afceeeaa6101}" cmd /K "%ALIASES% && %CMD_TOOLS%/ssh/' + serverName + '.bat"')
 
-    os.system('wt -p "{0caa0dad-35be-5f56-a8ff-afceeeaa6101}" cmd /K "%ALIASES%"')
-
-    return 'Complete'
-
-  def openUrl(self,url):
-    webbrowser.open(url)
-    WoxAPI.change_query(url)
-
-  #Following statement is necessary
-  if __name__ == "__main__":
-    Main()
+if __name__ == '__main__':
+  Main()
