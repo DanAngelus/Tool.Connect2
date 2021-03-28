55c7f0c6af1ae5ba6d52f80807f67b3f30845179
dbennett_tier2 <dbennett@tier2consulting.com>
2021-03-28 02:48:49 +0100

    [major] Major updates including listing options and support for password SSH

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from wox import Wox
-import os
 import json
 from subprocess import Popen
 
@@ -8,26 +7,62 @@ class Main(Wox):
   def loadServerSettings(tag):
     with open('ssh-servers.json') as servers_file:
       servers = json.load(servers_file)
-      return servers[tag]
+#       filtered_servers = list(filter(lambda s: s['tag'] == tag, servers))
+#       if filtered_servers:
+#         return filtered_servers[0]
+#       return {}
+      for s in servers:
+        if s['tag'] == tag:
+          return s
+      return {
+        "tag": "none",
+        "type": "ssh",
+        "description": f"Trying to find: {tag}",
+        "host": "dammit.son",
+        "port": "22",
+        "username": "tier2",
+        "password": '',
+        "certificate": "nocert"
+      }
+      # return list(filter(lambda s: s['tag'] == tag, servers))
 
-  def query(self, userInput):
-    # ToDo :: Load servers from file for auto-complete
-    return [{
-      'Title': 'Enter server tag',
-      'SubTitle': 'Connect to: ' + userInput,
+  def searchServerSettings(tag):
+    with open('ssh-servers.json') as servers_file:
+      servers = json.load(servers_file)
+      if not tag:
+        return servers
+      return list(filter(lambda s: s['tag'].startswith(tag), servers))
+
+  def generateItem(server):
+    return {
+      'Title': server['tag'],
+      'SubTitle': f"🔌 {server['description']}",
       'IcoPath': 'Images/pic.png',
       'JsonRPCAction': {
         'method': 'action',
-        'parameters': [userInput],
+        'parameters': [server['tag']],
         'dontHideAfterAction': False
       }
-    }]
+    }
+
+  def query(self, userInput):
+    if not userInput:
+      return [{
+        'Title': 'Server tag',
+        'SubTitle': 'Start typing a server tag...',
+        'IcoPath': 'Images/pic.png',
+        'JsonRPCAction': {
+          'method': 'action',
+          'parameters': [userInput],
+          'dontHideAfterAction': False
+        }
+      }]
+    return list(map(lambda server: Main.generateItem(server), Main.searchServerSettings(userInput)))
 
   def action(self, tag, commandType = 'ssh'):
     if commandType == 'ssh':
       with open('ssh-servers.json') as servers_file:
         server = Main.loadServerSettings(tag)
-
         if server:
           host = server['host']
           port = server['port']
@@ -37,17 +72,24 @@ class Main(Wox):
           description = server['description']
 
           # Assemble command string
-          command = f"echo Connecting to: {description} && ssh "
+          command = f"echo Connecting to: && echo {description} && echo "
+          if password:
+            command += f"Use password :: {password}"
+          else:
+            command += "... "
+
+          command += "&& ssh "
+
           if certificate:
             command += f"-i {certificate} "
 
+
           command += f"{username}@{host} -p {port}"
 
-#           Popen(["cmd", '/K', command ])
+          # ToDo :: Open into existing window
+          # https://docs.microsoft.com/en-us/windows/terminal/command-line-arguments?tabs=linux
           Popen(["wt", "new-tab", "-p", '"{0caa0dad-35be-5f56-a8ff-afceeeaa6101}"', 'cmd', '/K', f"{command}" ])
           return
-
-        print('No server found')
 
 if __name__ == '__main__':
   Main()
