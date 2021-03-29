caf5d76170670c9faac28bb39e45c44ff19ef88f
dbennett_tier2 <dbennett@tier2consulting.com>
2021-03-29 16:54:32 +0100

    [patch] Formatting

--- ConfigManager.py
+++ ConfigManager.py
@@ -1,20 +1,21 @@
 import json
 
+
 class ConfigManager():
     with open('settings.json') as settings:
         settings = json.load(settings)
 
-    def loadServerSettings(self, tag):
+    def load_server_settings(self, tag):
         with open(self.settings['serversConfigLocation']) as servers_file:
             servers = json.load(servers_file)
             return next(s for s in servers if s['tag'] == tag)
 
-    def searchServerSettings(self, tag):
+    def search_server_settings(self, tag):
         with open(self.settings['serversConfigLocation']) as servers_file:
             servers = json.load(servers_file)
             if not tag:
                 return servers
             return list(filter(lambda s: s['tag'].startswith(tag), servers))
 
-    def getKeysLocation(self):
+    def get_keys_location(self):
         return self.settings['sshKeysLocation']
--- Connect2.py
+++ Connect2.py
@@ -2,62 +2,63 @@ from wox import Wox
 from subprocess import Popen
 from ConfigManager import ConfigManager
 
+
 class Connect2(Wox):
+    configManager = ConfigManager()
 
-  configManager = ConfigManager()
-
-  def generateItem(self, server):
-    return {
-      'Title': server['tag'],
-      'SubTitle': f"🔌 {server['description']}",
-      'IcoPath': 'Images/pic.png',
-      'JsonRPCAction': {
-        'method': 'action',
-        'parameters': [server['tag']],
-        'dontHideAfterAction': False
-      }
-    }
-
-  def query(self, userInput):
-    if not userInput:
-      return [{
-        'Title': 'Server tag',
-        'SubTitle': 'Start typing a server tag...',
-        'IcoPath': 'Images/pic.png',
-        'JsonRPCAction': {
-          'method': 'action',
-          'parameters': [userInput],
-          'dontHideAfterAction': False
+    def generate_item(self, server):
+        return {
+            'Title': server['tag'],
+            'SubTitle': f"🔌 {server['description']}",
+            'IcoPath': 'Images/pic.png',
+            'JsonRPCAction': {
+                'method': 'action',
+                'parameters': [server['tag']],
+                'dontHideAfterAction': False
+            }
         }
-      }]
-    return list(map(lambda server: self.generateItem(server), self.configManager.searchServerSettings(userInput)))
 
-  def action(self, tag, commandType = 'ssh'):
-    if commandType == 'ssh':
-      server = self.configManager.loadServerSettings(tag)
-      if server:
-        host = server['host']
-        port = server['port']
-        username = server['username']
-        password = server['password']
-        certificate = server['certificate']
-        description = server['description']
+    def query(self, userInput):
+        if not userInput:
+            return [{
+                'Title': 'Server tag',
+                'SubTitle': 'Start typing a server tag...',
+                'IcoPath': 'Images/pic.png',
+                'JsonRPCAction': {
+                    'method': 'action',
+                    'parameters': [userInput],
+                    'dontHideAfterAction': False
+                }
+            }]
+        return list(map(lambda server: self.generate_item(server), self.configManager.search_server_settings(userInput)))
 
-        # Assemble command string
-        command = f"echo Connecting to: && echo {description} && echo "
-        if password:
-          command += f"Use password :: {password}"
-        else:
-          command += "... "
-        command += "&& ssh "
+    def action(self, tag, commandType='ssh'):
+        if commandType == 'ssh':
+            server = self.configManager.load_server_settings(tag)
+            if server:
+                host = server['host']
+                port = server['port']
+                username = server['username']
+                password = server['password']
+                certificate = server['certificate']
+                description = server['description']
 
-        if certificate:
-          command += f"-i {self.configManager.getKeysLocation()}/{certificate} "
+                # Assemble command string
+                command = f"echo Connecting to: && echo {description} && echo "
+                if password:
+                    command += f"Use password :: {password}"
+                else:
+                    command += "... "
+                command += "&& ssh "
 
-        command += f"{username}@{host} -p {port}"
+                if certificate:
+                    command += f"-i {self.configManager.get_keys_location()}/{certificate} "
 
-        Popen(["wt", "-f", "-p", '"{0caa0dad-35be-5f56-a8ff-afceeeaa6101}"', 'cmd', '/K', f"{command}" ])
-        return
+                command += f"{username}@{host} -p {port}"
+
+                Popen(["wt", "-f", "-p", '"{0caa0dad-35be-5f56-a8ff-afceeeaa6101}"', 'cmd', '/K', f"{command}"])
+                return
+
 
 if __name__ == '__main__':
-  Connect2()
+    Connect2()
